d6956fd1e2136e27cdf0af6459c8c6bc96a9cc15
Michael Kulinski <me@michaelkulinski.com>
2017-01-12 17:01:35 -0800

    creating state in App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,14 +6,29 @@ require('./App.scss');
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      day: ['Mon.', 'Tues.', 'Wed.', 'Thurs.', 'Fri.', 'Sat.', 'Sun.'],
+      jHackHour: [
+        { name: '', fellow: '', ass: true },
+        { name: '', fellow: '', ass: false },
+        { name: '', fellow: '', ass: false },
+      ],
+      sHackHour: [
+        { name: 'Add Linked Lists', fellow: 'Simon', date: '' },
+        { name: 'Super Balanced Tree', fellow: 'Simon' },
+        { name: 'Repeat Numbers', fellow: 'Simon' },
+      ],
+    };
   }
 
   render() {
     return (
         <div className="appContainer">
-          <Week />
+          <Week
+            {...this.state}
+          />
         </div>
-    )
+    );
   }
 }
 
