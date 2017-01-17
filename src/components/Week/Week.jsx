3d13ecd7a6767546384d9b7fd12113721c19ea9c
Michael Kulinski <me@michaelkulinski.com>
2017-01-16 18:54:41 -0800

    tests pass when scss is not imported

--- src/components/Week/Week.jsx
+++ src/components/Week/Week.jsx
@@ -1,9 +1,11 @@
 import React, { PropTypes } from 'react';
 
-require('./Week.scss');
+// require('./Week.scss');
 
 const Week = (props) =>
-  <div className="weekContainer">Week</div>;
+  <div className="weekContainer">
+    <h1>Week</h1>
+  </div>;
 
 Week.propTypes = {
 
--- test/week.spec.js
+++ test/week.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
 
-import Week from '../src/components/Week';
+import Week from '../src/components/Week/Week.jsx';
 
 describe('<Week />', () => {
   it('should have an h1', () => {
