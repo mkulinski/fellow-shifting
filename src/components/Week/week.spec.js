0986c37b6a439fc8dce2085a309dc7ff9c5eb5e4
Michael Kulinski <me@michaelkulinski.com>
2017-01-17 18:08:54 -0800

    added test files to week and app

---
+++ src/components/Week/week.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import Week from './Week.jsx';
+
+describe('<Week />', () => {
+  it('should have an h1', () => {
+    const wrapper = shallow(<Week />);
+    expect(wrapper.find('h1')).to.have.length(1);
+  });
+});
